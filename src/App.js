207f88c8a689fcd014cd20608a1a019986f49371
Fikri <91050135+Fikri-OK@users.noreply.github.com>
2023-11-06 20:02:12 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Car2Know Frontend ✌🖐!
+    </h1>
+  )
+}
+
+export default App;
