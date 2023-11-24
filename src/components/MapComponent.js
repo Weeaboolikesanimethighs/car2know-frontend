9f0d9bb2076fd5470fe55de8ddbfa71ce25877c1
Fikri <91050135+Fikri-OK@users.noreply.github.com>
2023-11-24 18:11:49 +0100

    MapComponent tried

---
+++ src/components/MapComponent.js
@@ -0,0 +1,19 @@
+/*import React from 'react';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+
+const MapComponent = () => {
+  const center = [48.198517, 16.322743]; // Coordinates for Ganglbauergasse 24, 1160 Wien
+
+  return (
+    <MapContainer center={center} zoom={15} style={{ width: '100px', height: '100px' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={center} />
+    </MapContainer>
+  );
+};
+
+export default MapComponent;
+*/
