acd9be59617ec7373d50c44336b7aadf33f7d7b1
Fikri <oksum.fikri@gmail.com>
2023-11-27 13:08:49 +0100

    Update User.js

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,28 +1,15 @@
 import React from "react";
 import Task from "../components/Task";
-<<<<<<< Updated upstream
 import LogoutButton from '../components/LogoutButton'; // Import the LogoutButton component
+import Details from '../components/Details'
 import { useAuth } from '../contexts/AuthContext';
 
-=======
-import Details from "../components/Details";
->>>>>>> Stashed changes
 
 const User = () => {
 
   const { username, isAuthenticated  } = useAuth();
 
   return (
-<<<<<<< Updated upstream
-
-    <div>
-      <h2>Welcome, {username}!</h2>
-      <Task status="Completed" name="Mirac Fidanci" />
-      <Task status="In Progress" name="Fikri Öksum" />
-      <Task status="Not Started" name="Gabriel Montemayor" />
-      {isAuthenticated && <LogoutButton />}
-
-=======
     <div className="grid grid-cols-2">
       <div>
         <Task status="Completed" name="Mirac Fidanci" />
@@ -32,7 +19,6 @@ const User = () => {
       <div>
         <Details />
       </div>
->>>>>>> Stashed changes
     </div>
   );
 };
