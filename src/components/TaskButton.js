26f38d1383f2573a495f0d3b787fe6fc9e5bec59
Fikri <oksum.fikri@gmail.com>
2023-11-29 19:49:21 +0100

    Create TaskButton.js

---
+++ src/components/TaskButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const TaskButton = () => {
+    <div>
+        <button className="bg-white rounded-full p-4 hover:bg-gray-200 focus:outline-none">
+            <div className="w-6 h-6 border-t-2 border-gray-500 mx-auto"></div>
+            <div className="w-6 h-6 border-r-2 border-gray-500 my-auto"></div>
+        </button>
+    </div>
+};
+
+export default TaskButton;
