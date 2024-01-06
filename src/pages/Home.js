ae8a904425881ec751173fe0dfb12a627734272a
Fikri <91050135+Fikri-OK@users.noreply.github.com>
2024-01-06 14:21:57 +0100

    cookiebanner added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import AddCarButton from '../components/AddCarButton';
+import CookieConsent from "react-cookie-consent";
 
 const Home = () => {
   return (
     <div>
       <h1>Car2Know 🚗</h1>
       <AddCarButton/>
+
+      <CookieConsent>This website uses cookies to enhance the user experience.</CookieConsent>
     </div>
   );
 };
